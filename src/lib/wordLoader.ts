97d0f94d9b40b229f8ad76f6b00157aa015d5ab4
Nielda Karla <90112119+nieldakarla@users.noreply.github.com>
2026-05-15 12:12:33 -0300

    Expand Italian word list

--- src/lib/wordLoader.ts
+++ src/lib/wordLoader.ts
@@ -43,7 +43,7 @@ export async function drawRound(language: Language): Promise<Word[]> {
         notYetDueWords.push({ word, dueDate: due })
       }
     } else {
-      newWords.push(word) // never seen, ordered by rank (most frequent first)
+      newWords.push(word) // never seen, ordered by file position
     }
   }
 
